4496dedb703a41f8f04b8e0e2a4f06c69389fc0d
AnastasiaLd <32487197+AnastasiaLd@users.noreply.github.com>
2019-07-27 17:15:17 +0100

    Create block_confluence_guide_finder.php

---
+++ lang/en/block_confluence_guide_finder.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Strings for component 'bblock_confluence_guide_finder', language 'en'
+ *
+ * @package   block_confluence_guide_finder
+ * @copyright 2019, 2019, London School of Economics
+ */
+
+$string['blockfooter'] = 'Custom block footer';
+$string['blockfooter_desc'] = 'Choose a custom block footer default message.';
+$string['confluencedomain'] = 'Server Base URL';
+$string['confluencedomain_desc'] = 'Example: http://myconfluencesite.net/wiki ';
+$string['confluenceguidefinder'] = 'User guides finder';
+$string['connectionerror'] = 'Could not connect to Confluence. Contact the site administrator';
+$string['confluencespacekey'] = 'Space Key';
+$string['confluencespacekey_desc'] = 'If more than one, separate each space key with a comma';
+$string['notfoundmessage'] = 'Sorry, no results were found';
+$string['pluginname'] = 'Guides finder';

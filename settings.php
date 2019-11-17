d16855217e8af916a03228fe2f384855adae13ca
AnastasiaLd <32487197+AnastasiaLd@users.noreply.github.com>
2019-11-17 15:42:02 +0000

    Removed space key setting

--- settings.php
+++ settings.php
@@ -13,9 +13,6 @@ $settings->add( new admin_setting_configtext( 'block_confluence_guide_finder/con
                                               get_string( 'confluencedomain', 'block_confluence_guide_finder' ),
                                               get_string( 'confluencedomain_desc', 'block_confluence_guide_finder' ), '' ) );
 
-$settings->add( new admin_setting_configtext( 'block_confluence_guide_finder/confluencespacekey',
-                                              get_string('confluencespacekey', 'block_confluence_guide_finder'),
-                                              get_string('confluencespacekey_desc', 'block_confluence_guide_finder'), '' ));
 
 $settings->add(new admin_setting_confightmleditor( 'block_confluence_guide_finder/blockfooter',
                                                     get_string( 'blockfooter', 'block_confluence_guide_finder' ),

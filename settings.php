b3ffd1aaa67c628e3549007337fafb71b137c434
AnastasiaLd <32487197+AnastasiaLd@users.noreply.github.com>
2019-11-22 23:41:55 +0000

    Added url validation
    Added url validation for confluence domain

--- settings.php
+++ settings.php
@@ -11,7 +11,7 @@ defined('MOODLE_INTERNAL') || die;
 
 $settings->add( new admin_setting_configtext( 'block_confluence_guide_finder/confluencedomain',
                                               get_string( 'confluencedomain', 'block_confluence_guide_finder' ),
-                                              get_string( 'confluencedomain_desc', 'block_confluence_guide_finder' ), '' ) );
+                                              get_string( 'confluencedomain_desc', 'block_confluence_guide_finder' ), '', PARAM_URL ) );
 
 
 $settings->add(new admin_setting_confightmleditor( 'block_confluence_guide_finder/blockfooter',

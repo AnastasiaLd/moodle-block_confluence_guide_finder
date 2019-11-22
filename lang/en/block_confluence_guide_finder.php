8f5762d1330b572e1f41f1e6a499410b37ced288
AnastasiaLd <32487197+AnastasiaLd@users.noreply.github.com>
2019-11-22 23:59:44 +0000

    Fixed description for confluence domain field

--- lang/en/block_confluence_guide_finder.php
+++ lang/en/block_confluence_guide_finder.php
@@ -10,7 +10,7 @@
 $string['blockfooter'] = 'Custom block footer';
 $string['blockfooter_desc'] = 'Choose a custom block footer default message.';
 $string['confluencedomain'] = 'Server Base URL';
-$string['confluencedomain_desc'] = 'Example: http://myconfluencesite.net/wiki ';
+$string['confluencedomain_desc'] = 'Example: http://myconfluencesite.net/wiki/ ';
 $string['confluenceguidefinder'] = 'Guides finder';
-$string['notfoundmessage'] = 'Sorry, no results were found';
+$string['notfoundmessage'] = 'Sorry, no results were found.';
 $string['pluginname'] = 'Guides finder';
